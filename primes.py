990d8cda393a9edc95e4b7a328e9a3dd265ab217
Nouf Aldossary <nouf.aldossary@kcl.ac.uk>
2023-12-08 03:34:16 +0000

    passed all tests

--- primes.py
+++ primes.py
@@ -2,5 +2,16 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
+
+    if number_of_primes <= 0:
+        raise ValueError("You must pass a positive number to generate prime list")
+
+    list = [2]
+    i = 3
+    while len(list) < number_of_primes:
+        if( (i % 3 != 0 and i % 2 == 1 and i % 5 != 0 and i % 7 != 0) or i==3 or i ==5 or i ==7):
+            list.append(i)
+        
+        i += 2   
+
     return list
